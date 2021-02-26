be3c29d30b41ae178057a536461bccc68e30046a
etienneoceane <etienne.oceane@outlook.com>
2021-02-26 12:37:43 +0100

    commit du 26.02

--- React_test/src/App.js
+++ React_test/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react'
-import Home from './pages/Home'
+import Banner from './components/Banner'
+// import Cart from './components/Cart'
+import ShoppingList from'./components/ShoppingList'
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <Home/>
-    </div>
-  );
-}
+        return (
+          <div className="App">
+            <Banner/>
+            {/* <Cart/> */}
+            <ShoppingList/>
+          </div>
+        );
+      }
 
 export default App;
